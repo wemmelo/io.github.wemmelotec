7a70c3f7716c71aebcf6ef6a03352a1371057c86
wellington <wellington.magno@gmail.com>
2020-03-29 22:02:19 -0300

    last of class hello world

--- src/main/java/io/github/wemmelotec/VendasApplication.java
+++ src/main/java/io/github/wemmelotec/VendasApplication.java
@@ -2,10 +2,22 @@ package io.github.wemmelotec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 //Essa anotação faz com que o Spring reconheça que essa classe vai ser responsavél
 //por levantar minha aplicação
 @SpringBootApplication
+@RestController
+/*
+Essa notação restcontroller basicamente informa ao spring que essa classe é um controlador rest
+ou seja, ela vai enviar mensagens ao browser
+ */
 public class VendasApplication {
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello World";
+    }
     public static void main(String[] args) {
         //comando para inicializar a aplicação spring boot
         //o método run recebe dois parametros
